2549532793807d042e1cf002fc6ac106de10125c
Onur Yıldırım <onur.yildirim.2017@ogr.iu.edu.tr>
2022-02-19 00:01:02 +0300

    Update MainPage.java

--- MainPage.java
+++ MainPage.java
@@ -2,7 +2,7 @@ public class MainPage {
 	
 	public static void main(String[] args) {
 	
-		//Algorithm to find the ones digit, tens digit and hundreds digit of a 3-digit integer.
+		//Algorithm to find the units digit, tenth digit and hundreds digit of a 3-digit integer.
 		Algorithm algorithm = new Algorithm();
 		algorithm.startCalculate();
 	
