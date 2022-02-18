dee0da2ea745e9ad47e526dbd6c5eed3a9c71d68
Onur Yıldırım <onur.yildirim.2017@ogr.iu.edu.tr>
2022-02-18 23:45:45 +0300

    Create MainPage.java

---
+++ MainPage.java
@@ -0,0 +1,11 @@
+public class MainPage {
+	
+	public static void main(String[] args) {
+	
+		//Algorithm to find the ones digit, tens digit and hundreds digit of a 3-digit integer.
+		Algorithm algorithm = new Algorithm();
+		algorithm.startCalculate();
+	
+	}
+
+}
